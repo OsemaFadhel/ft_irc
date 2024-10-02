6cd91f65b4396be88f4cabcd9f873c7225ee39b6
Lorenzo <lorenzonicotera02@gmail.com>
2024-10-02 16:26:10 +0200

    test

--- srcs/server/commands/Join.cpp
+++ srcs/server/commands/Join.cpp
@@ -6,7 +6,7 @@
 /*   By: lnicoter <lnicoter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:38:08 by lnicoter          #+#    #+#             */
-/*   Updated: 2024/10/01 17:21:07 by lnicoter         ###   ########.fr       */
+/*   Updated: 2024/10/02 15:57:02 by lnicoter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 //substr e find per spostare il puntatore alla prima virgola
 //new error: 2 channels that are going to be inserted with
-//a space after the first , for example <channel1>, <channel2> the space messes up all the logic
+//a space after the first , for example <channel1>, <channel2> the space messes up all the logic [SOLVED]
 std::vector<std::string>	Server::channelParser(std::string args)
 {
 
@@ -58,7 +58,6 @@ std::vector<std::string>	Server::channelParser(std::string args)
 			std::cout << "Invalid channel ignored: " << temp << std::endl;
 		}
 	}
-	//why reach the end of this where a lot of channels are inserted
 	return (numOfChannels);
 }
 
@@ -67,31 +66,44 @@ std::vector< std::string >	Server::keyParser(std::string args)
 {
 	std::vector < std::string > keys;
 	size_t	i = (args.find_first_of(" ")) + 1;
+	std::string temp;
+
 	//loop for keys
 	while (i != std::string::npos)
 	{
 		size_t pos = args.find_first_of(",", i);
 		if (pos != std::string::npos)
 		{
-			std::string temp = args.substr(i, pos - i);
+			temp = args.substr(i, pos - i);
 			keys.push_back(temp);
 			i = pos + 1;
 		}
 		else
 		{
-			std::string temp = args.substr(i, pos - i);
+			temp = args.substr(i, pos - i);
 			keys.push_back(temp);
 			break;
 		}
+
+		temp.erase(0, temp.find_first_not_of(" "));
+		temp.erase(temp.find_last_not_of(" ") + 1);
+
+		// Controlla se la stringa è valida (non vuota e non contiene spazi)
+		if (!temp.empty() && temp.find(' ') == std::string::npos)
+		{
+			keys.push_back(temp);
+			std::cout << "Key added: " << temp << std::endl;
+		}
+		else
+		{
+			std::cout << "Invalid Key ignored: " << temp << std::endl;
+		}
 	}
+
 	for (size_t i = 0; i < keys.size(); i++)
 		std::cout << "keys: " << keys[i] << std::endl;
 	return (keys);
 }
-
-//Troubleshooting:
-//
-
 
 
 
@@ -135,15 +147,39 @@ void	Server::checkChannelExist(std::vector< std::string > numberOfChannels, Clie
 }
 
 
+/*
+** Troubleshooting
+** The parsing of the keys is on point and it works very well
+** I should do the same checks for the keyparser
+** Following another irc it seems the keys are ignored if the channel doesn't exist and it's
+** the first time
+
+** I must create a channel section in the irc client reference
+** the answers are hidden in the RFC
+
+
+   If a JOIN is successful, the user receives a JOIN message as
+   confirmation and is then sent the channel's topic (using RPL_TOPIC) and
+   the list of users who are on the channel (using RPL_NAMREPLY), which
+   MUST include the user joining.
+
+! the modes defines if there's a password or not
+
+*/
+
 void	Server::Join(std::string args, int	clientSocket)
 {
 	Client	*clientToInsert = getClient(clientSocket);
 	std::vector < std::string > numOfChannels;
-	// std::vector < std::string > keys;
+	std::vector < std::string > keys;
 
 	numOfChannels = channelParser(args);
 	//the keys works only if the channel already exists
-	// keys = keyParser(args);
+	keys = keyParser(args);
+	for (size_t k = 0; k < keys.size(); k++)
+		std::cout << "keys: " << keys[k] << std::endl;
+
 	checkChannelExist(numOfChannels, *clientToInsert);
 	channelCheck();
 }
+
